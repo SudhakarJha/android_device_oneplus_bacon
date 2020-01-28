f2dbe4c4f17f99f8dfcc3a63e9fcf31c5d1fbeb7
Sudhakar Jha <sudhakar.jha@gmail.com>
2020-01-28 13:12:07 +1300

    Inherit from msm8974-common

--- bacon.mk
+++ bacon.mk
@@ -20,6 +20,9 @@ $(call inherit-product, vendor/oneplus/bacon/bacon-vendor.mk)
 # Inherit our OneplusShit
 $(call inherit-product, device/oneplus/OneplusShit/opshit.mk)
 
+# Inherit from msm8974-common
+$(call inherit-product, device/oppo/msm8974-common/msm8974.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
